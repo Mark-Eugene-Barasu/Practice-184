4404b379c143cc8b26fc8b7fc290a9e3e6563135
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-09-26 19:46:40 +0200

    fiddling with Node.js Express.js

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 const fs = require("fs");
+const express = require("express");
 
-fs.copyFileSync(`main.js`, `second.js`);
+
+// fs.copyFileSync(`main.js`, `second.js`);
+
+const app = express();
+
+app.get("/", function (req, res) {
+    
+    console.log(req);
+    console.log(res);
+    res.send(`I love candy`);
+
+});
+
+
+app.listen(3000, function () {
+    console.log(`Running on port:3000`);    
+});
+
+
