f5600f9b35df62343cc73bcf12628d4e93c79b68
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-09-27 06:19:03 +0200

    self assignment SUCCESSFUL

--- main.js
+++ main.js
@@ -1,25 +1,36 @@
+// jshint esversion:6
 
-import { copyFileSync, unlinkSync } from "fs";
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+import url from "url";
+import path from "path";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+// const express = require("express");
 import express from "express";
 
-
-copyFileSync(`./main.js`, `./second.js`);
-unlinkSync(`./second.js`);
-
+// const bodyParser = require("body-parser");
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true})); // to post nested objects
+
 
 app.get("/", function (req, res) {
-    
-    console.log(req);
-    console.log(res);
-    res.send(`I love candy`);
-
+    res.sendFile(`${__dirname}/index.html`);
 });
 
+app.post("/", function (req, res) {
+    console.log(req.body);
+    res.send(`<h2>Welcome, ${req.body.firstName} ${req.body.middleName} ${req.body.lastName}.</h2> <br> We believe <a href="mailto:mark0630227248@gmail.com">${req.body.userEmail}</a> is your email address.`);
+})
 
-app.listen(3000, function () {
-    console.log(`Running on port:3000`);    
-});
+
+app.listen(3000, () => {
+        console.log(`Running on port:3000`);
+    });
 
 
